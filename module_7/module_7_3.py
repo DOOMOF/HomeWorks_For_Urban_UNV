ffc0ad5a146e69fa0a24c5e6d9857b0983b4d0c8
DOOMOF <dumovegor1@gmail.com>
2024-10-21 14:36:50 +0700

    module_7_3.py

--- module_7/module_7_3.py
+++ module_7/module_7_3.py
@@ -1,20 +1,25 @@
-import re
+import string
 
 
 class WordsFinder:
+
     def __init__(self, *file_names: list):
         self.file_names = file_names
 
     def get_all_words(self):
         all_words = {}
-        words = []
-        for file in self.file_names:
-            with open(file, encoding= 'utf-8') as file_now:
-                for line in file_now:
-                    parts = re.split(r'[ \n,.=!?;:-]', line)
-                    words.extend(filter(None, parts))
-                all_words[file] = words
-            return all_words
+        for file_name in self.file_names:
+            try:
+                with open(file_name, 'r', encoding='utf-8') as file_now:
+                    words = []
+                    for line in file_now:
+                        line = line.lower()
+                        line = line.translate(str.maketrans('', '', string.punctuation + '—'))
+                        words.extend(line.split())
+                all_words[file_name] = words
+            except FileNotFoundError:
+                print(f'Файл{file_name} не найден')
+        return all_words
     def find(self, word):
         for name, words in self.get_all_words().items():
             find_words = {}
@@ -23,6 +28,7 @@ class WordsFinder:
                 if j.lower() == word.lower():
                     find_words[name] = i
                     return find_words
+                    break
                 i += 1
 
     def count(self, word):
@@ -35,9 +41,8 @@ class WordsFinder:
             find_words[name] = i
         return find_words
 
-finder2 = WordsFinder('text.txt')
+finder2 = WordsFinder('test_file.txt')
 print(finder2.get_all_words()) # Все слова
 print(finder2.find('TEXT')) # 3 слово по счёту
 print(finder2.count('teXT')) # 4 слова teXT в тексте всего
-
 
