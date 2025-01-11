1ec1401cc8f849ebcd4588ab8eecd1d59892d12b
DOOMOF <dumovegor1@gmail.com>
2025-01-11 17:58:32 +0700

    Update module_12_2.py

--- module_12/module_12_2.py
+++ module_12/module_12_2.py
@@ -2,41 +2,90 @@ import unittest
 
 
 class Runner:
-    def __init__(self, name):
+    def __init__(self, name, speed=5):
         self.name = name
         self.distance = 0
+        self.speed = speed
 
     def run(self):
-        self.distance += 10
+        self.distance += self.speed * 2
 
     def walk(self):
-        self.distance += 5
+        self.distance += self.speed
 
     def __str__(self):
         return self.name
 
-
-class RunnerTest(unittest.TestCase):
-    def test_walk(self):
-        runner = Runner("TestRunner")
-        for _ in range(10):
-            runner.walk()
-        self.assertEqual(runner.distance, 50)
-
-    def test_run(self):
-        runner = Runner("TestRunner")
-        for _ in range(10):
-            runner.run()
-        self.assertEqual(runner.distance, 100)
-
-    def test_challenge(self):
-        runner1 = Runner("Runner1")
-        runner2 = Runner("Runner2")
-        for _ in range(10):
-            runner1.run()
-            runner2.walk()
-        self.assertNotEqual(runner1.distance, runner2.distance)
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
 
 
-if __name__ == '__main__':
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
+
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.all_result = {}
+
+    def setUp(self):
+        self.runner1 = Runner("Усейн", speed=10)
+        self.runner2 = Runner("Андрей", speed=9)
+        self.runner3 = Runner("Ник", speed=3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for key, result in cls.all_result.items():
+            formatted_result = {place: str(runner) for place, runner in result.items()}
+            print(formatted_result)
+
+    def test_usein_and_nik(self):
+        tournament = Tournament(90, self.runner1, self.runner3)
+        results = tournament.start()
+        TournamentTest.all_result["Усейн и Ник"] = results
+        last_place = max(results.keys())
+        self.assertTrue(results[last_place] == "Ник")
+
+    def test_andrey_and_nik(self):
+        tournament = Tournament(90, self.runner2, self.runner3)
+        results = tournament.start()
+        TournamentTest.all_result["Андрей и Ник"] = results
+        last_place = max(results.keys())
+        self.assertTrue(results[last_place] == "Ник")
+
+    def test_usein_andrey_nik(self):
+        tournament =  Tournament(90, self.runner2, self.runner1, self.runner3)
+        results = tournament.start()
+        TournamentTest.all_result["Усейн, Андрей и Ник"] = results
+        last_place = max(results.keys())
+        self.assertTrue(results[last_place] == "Ник")
+
+
+if __name__ == "__main__":
     unittest.main()
+
+
+
+
+
+
+
